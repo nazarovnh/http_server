0cad51a1dd8d04efe2b0036a953a488d8b700972
Nariman Nazarov <nazarov.nh@gmail.com>
2022-05-30 16:57:01 +0600

    package project with maven

---
+++ src/main/java/it/sevenbits/http/server/Main.java
@@ -0,0 +1,19 @@
+package it.sevenbits.http.server;
+
+/**
+ * main.java.it.sevenbits.practice.Main application entry point
+ */
+final  class Main {
+    private Main(){
+
+    }
+    /**
+     * main.java.it.sevenbits.practice.Main function for app
+     *
+     * @param args - console arguments
+     */
+    public static void main(final String[] args) {
+
+            System.out.println("Hello, world!");
+    }
+}
